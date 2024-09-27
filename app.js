937467caf0e8f9ed3f59517feb7a9c3b520df94d
eihodge <ethaningramh@gmail.com>
2024-09-27 01:02:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry);
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
         }
     });
+}, {
+    rootMargin: '0px 0px 360px 0px' // Adjusts when elements come into view
 });
 
 const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el))
+hiddenElements.forEach((el) => observer.observe(el));
+
