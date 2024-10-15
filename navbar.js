c08f2989c6eea276e8cfaa98559d35a46b31423a
eihodge <ethaningramh@gmail.com>
2024-10-15 04:20:15 -0400

    bug fix in navbar

--- navbar.js
+++ navbar.js
@@ -1,7 +1,7 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('website-links')[0]
 
-toggleButton.addEventListener('click', () => {
+toggleButton.addEventListener('click', (event) => {
   event.preventDefault();
   navbarLinks.classList.toggle('active');
   toggleButton.classList.toggle('open');
