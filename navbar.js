f06c62fc43ffe749096326649af9004982ed6def
eihodge <ethaningramh@gmail.com>
2024-10-15 04:19:30 -0400

    add dropdown animation and navbar bug fix

--- navbar.js
+++ navbar.js
@@ -2,6 +2,7 @@ const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('website-links')[0]
 
 toggleButton.addEventListener('click', () => {
+  event.preventDefault();
   navbarLinks.classList.toggle('active');
   toggleButton.classList.toggle('open');
 })
